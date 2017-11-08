2c0b566282870b94bb81653e2017c1a3d1d465d0
Michalis Antoniou <MAntoniou@CircleLinkHealth.com>
2017-11-08 02:15:05 +0100

    Fix

--- src/Cerberus/Middleware/CerberusRole.php
+++ src/Cerberus/Middleware/CerberusRole.php
@@ -37,13 +37,13 @@ class CerberusRole
      *
      * @return mixed
      */
-	public function handle($request, Closure $next, $roles, $site)
+	public function handle($request, Closure $next, $roles)
 	{
 		if (!is_array($roles)) {
 			$roles = explode(self::DELIMITER, $roles);
 		}
 
-		if ($this->auth->guest() || !$request->user()->hasRole($roles, false, $site)) {
+		if ($this->auth->guest() || !$request->user()->hasRole($roles, false)) {
 			abort(403);
 		}
 
